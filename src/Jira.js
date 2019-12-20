9f619c4f2fa2aed54a87d568e7e35a957b85a109
Connor Smallman <connor@kodify.io>
2019-12-20 15:05:45 +0000

    Adds oauth to jire client

--- src/Jira.js
+++ src/Jira.js
@@ -1,7 +1,7 @@
 import "@babel/polyfill";
-import JiraApi from 'jira-client';
-import PromiseThrottle from 'promise-throttle';
-import Slack from './Slack';
+import JiraApi from "jira-client";
+import PromiseThrottle from "promise-throttle";
+import Slack from "./Slack";
 
 const promiseThrottle = new PromiseThrottle({
   requestsPerSecond: 10,
@@ -12,7 +12,6 @@ const promiseThrottle = new PromiseThrottle({
  * Generate changelog by matching source control commit logs to jiar tickets.
  */
 export default class Jira {
-
   constructor(config) {
     this.config = config;
     this.slack = new Slack(config);
@@ -20,32 +19,47 @@ export default class Jira {
     this.releaseVersions = [];
     this.ticketPromises = {};
 
-    const { host, username, password} = config.jira.api;
-    let { email, token } = config.jira.api;
-
-    if (!token && typeof password !== 'undefined') {
-      console.warn('WARNING: Jira password is deprecated. Use an API token instead.');
-      token = password
-    }
-    if (!email && typeof username !== 'undefined') {
-      console.warn('WARNING: Jira username is deprecated for API authentication. Use user email instead.');
-      email = username
-    }
-
     if (config.jira.api.host) {
-      this.jira = new JiraApi({
-        host,
-        username: email,
-        password: token,
-        protocol: 'https',
-        apiVersion: 2,
-        strictSSL: true
-      });
+      if (config.jira.api.oauth) {
+        this.jira = new JiraApi({
+          host,
+          protocol: "https",
+          apiVersion: 2,
+          strictSSL: true,
+          oath
+        });
+      } else {
+        const { host, username, password } = config.jira.api;
+        let { email, token } = config.jira.api;
+
+        if (!token && typeof password !== "undefined") {
+          console.warn(
+            "WARNING: Jira password is deprecated. Use an API token instead."
+          );
+          token = password;
+        }
+        if (!email && typeof username !== "undefined") {
+          console.warn(
+            "WARNING: Jira username is deprecated for API authentication. Use user email instead."
+          );
+          email = username;
+        }
+
+        this.jira = new JiraApi({
+          host,
+          username: email,
+          password: token,
+          protocol: "https",
+          apiVersion: 2,
+          strictSSL: true
+        });
+      }
     } else {
-      console.error('ERROR: Cannot configure Jira without a host configuration.');
+      console.error(
+        "ERROR: Cannot configure Jira without a host configuration."
+      );
     }
   }
-
   /**
    * Generate changelog by matching source control commit logs to jira tickets
    * and, optionally, creating the release version.
@@ -58,33 +72,37 @@ export default class Jira {
     const logs = [];
     this.releaseVersions = [];
     try {
-
-      const promises = commitLogs.map((commit) => {
-        return this.findJiraInCommit(commit, releaseVersion)
-          .then((log) => { logs.push(log); });
+      const promises = commitLogs.map(commit => {
+        return this.findJiraInCommit(commit, releaseVersion).then(log => {
+          logs.push(log);
+        });
       });
       promises.push(Promise.resolve()); // ensure at least one
 
-      return Promise.all(promises)
-        // Add to release version
-        .then(() => {
+      return (
+        Promise.all(promises)
+          // Add to release version
+          .then(() => {
+            // Get all Jira tickets (filter out duplicates by keying on ID)
+            let ticketsHash = {};
+            let ticketsList = [];
+            logs.forEach(log => {
+              log.tickets.forEach(ticket => (ticketsHash[ticket.id] = ticket));
+            });
+            ticketsList = Object.keys(ticketsHash).map(k => ticketsHash[k]);
 
-          // Get all Jira tickets (filter out duplicates by keying on ID)
-          let ticketsHash = {};
-          let ticketsList = [];
-          logs.forEach((log) => {
-            log.tickets.forEach(ticket => ticketsHash[ticket.id] = ticket);
-          });
-          ticketsList = Object.keys(ticketsHash).map(k => ticketsHash[k]);
+            // If there are Jira tickets, create a release for them
+            if (ticketsList.length && releaseVersion) {
+              return this.addTicketsToReleaseVersion(
+                ticketsList,
+                releaseVersion
+              ).then(() => logs);
+            }
 
-          // If there are Jira tickets, create a release for them
-          if (ticketsList.length && releaseVersion) {
-            return this.addTicketsToReleaseVersion(ticketsList, releaseVersion).then(() => logs);
-          }
-
-          return logs;
-        });
-    } catch(e) {
+            return logs;
+          })
+      );
+    } catch (e) {
       throw new Error(e);
     }
   }
@@ -98,25 +116,27 @@ export default class Jira {
    * @return {Promsie} Resolves to an object with a jira array property
    */
   findJiraInCommit(commitLog) {
-    const log = Object.assign({tickets: []}, commitLog);
+    const log = Object.assign({ tickets: [] }, commitLog);
     const promises = [];
     const found = {};
 
     const configPattern = this.config.jira.ticketIDPattern;
-    const ticketPattern = new RegExp(configPattern.source, configPattern.flags.replace('g', ''));
+    const ticketPattern = new RegExp(
+      configPattern.source,
+      configPattern.flags.replace("g", "")
+    );
 
     // Search for jira ticket numbers in the commit text
     const tickets = this.getTickets(log);
-    tickets.forEach((ticketMatch) => {
-
+    tickets.forEach(ticketMatch => {
       // Get the ticket key, and skip loading if we already got this one
       let key = ticketMatch.match(ticketPattern);
-      key = (key.length > 1) ? key[1] : key[0];
+      key = key.length > 1 ? key[1] : key[0];
       if (!key) {
         return;
       }
       key = key.toUpperCase();
-      if (found[key]){
+      if (found[key]) {
         return;
       }
       found[key] = true;
@@ -146,9 +166,7 @@ export default class Jira {
     // Get Jira issue ticket object
     let promise = this.ticketPromises[ticketKey];
     if (!promise) {
-      promise = promiseThrottle.add(
-        this.getJiraIssue.bind(this, ticketKey)
-      );
+      promise = promiseThrottle.add(this.getJiraIssue.bind(this, ticketKey));
       promise.catch(() => {
         console.log(`Ticket ${ticketKey} not found`);
       });
@@ -156,7 +174,7 @@ export default class Jira {
     }
 
     // Add to commit
-    promise.then((ticket) => {
+    promise.then(ticket => {
       if (this.includeTicket(ticket)) {
         commitLog.tickets.push(ticket);
         return ticket;
@@ -190,30 +208,32 @@ export default class Jira {
         versionPromises[project] = verPromise;
 
         // Add to list of releases
-        verPromise.then((ver) => {
+        verPromise.then(ver => {
           ver.projectKey = project;
           this.releaseVersions.push(ver);
         });
       }
 
       // Add version to ticket
-      return verPromise
-        .then((versionObj) => {
-          const { fixVersions} = ticket.fields;
-          fixVersions.push({ name: versionObj.name });
-          return this.jira.updateIssue(ticket.id, {
-            fields: { fixVersions }
-          })
+      return verPromise.then(versionObj => {
+        const { fixVersions } = ticket.fields;
+        fixVersions.push({
+          name: versionObj.name
         });
+        return this.jira.updateIssue(ticket.id, {
+          fields: { fixVersions }
+        });
+      });
     }
-
     // Loop through tickets and throttle the promises.
-    const promises = tickets.map((ticket) => {
+    const promises = tickets.map(ticket => {
       return promiseThrottle
         .add(updateTicketVersion.bind(this, ticket))
-        .catch((err) => {
-          console.log(JSON.stringify(err, null, '  '));
-          console.log(`Could not assign ticket ${ticket.key} to release '${versionName}'!`);
+        .catch(err => {
+          console.log(JSON.stringify(err, null, "  "));
+          console.log(
+            `Could not assign ticket ${ticket.key} to release '${versionName}'!`
+          );
         });
     });
     return Promise.all(promises);
@@ -250,17 +270,21 @@ export default class Jira {
    */
   async getJiraIssue(ticketId) {
     if (!this.jira) {
-      return Promise.reject('Jira is not configured.');
+      return Promise.reject("Jira is not configured.");
     }
 
-    return this.jira.findIssue(ticketId).then((origTicket) => {
+    return this.jira.findIssue(ticketId).then(origTicket => {
       const ticket = Object.assign({}, origTicket);
-      return this.slack.findUser(ticket.fields.reporter.emailAddress, ticket.fields.reporter.displayName)
-      .then((slackUser) => {
-        ticket.slackUser = slackUser;
-        return ticket;
-      })
-      .catch(() => ticket);
+      return this.slack
+        .findUser(
+          ticket.fields.reporter.emailAddress,
+          ticket.fields.reporter.displayName
+        )
+        .then(slackUser => {
+          ticket.slackUser = slackUser;
+          return ticket;
+        })
+        .catch(() => ticket);
     });
   }
 
@@ -271,10 +295,12 @@ export default class Jira {
    */
   includeTicket(ticket) {
     const type = ticket.fields.issuetype.name;
-    if (Array.isArray(this.config.jira.includeIssueTypes) && this.config.jira.includeIssueTypes.length) {
+    if (
+      Array.isArray(this.config.jira.includeIssueTypes) &&
+      this.config.jira.includeIssueTypes.length
+    ) {
       return this.config.jira.includeIssueTypes.includes(type);
-    }
-    else if (Array.isArray(this.config.jira.excludeIssueTypes)) {
+    } else if (Array.isArray(this.config.jira.excludeIssueTypes)) {
       return !this.config.jira.excludeIssueTypes.includes(type);
     }
   }
@@ -286,7 +312,10 @@ export default class Jira {
    */
   getTickets(log) {
     const configPattern = this.config.jira.ticketIDPattern;
-    const searchPattern = new RegExp(configPattern.source, `${configPattern.flags || ''}g`);
+    const searchPattern = new RegExp(
+      configPattern.source,
+      `${configPattern.flags || ""}g`
+    );
     return log.fullText.match(searchPattern) || [];
   }
 }
